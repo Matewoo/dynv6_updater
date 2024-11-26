5165761738ba3d510b1f1d8f11fc9fa4f08b4716
Mateo Körber Belmonte <44673490+Matewoo@users.noreply.github.com>
2024-11-26 17:33:45 +0100

    poc works

--- ddns_updater.py
+++ ddns_updater.py
@@ -1,8 +1,24 @@
 import requests
 
+dynv6_username = "" # Example: "55cjZFwRJ7URYRchE8WY5UHdVRnB5l"
+dynv6_domain = "" # Example: "dynv6.net"
+
 def get_public_ip():
     response = requests.get("https://api.ipify.org?format=text")
     return response.text
 
+def call_api():
+    
+    try:
+        response = requests.get(f"https://dynv6.com/api/update?hostname={dynv6_domain}&token={dynv6_username}&ipv4=" + get_public_ip())
+        response.raise_for_status()
+        return response.text
+    except requests.RequestException as e:
+        print(f"Fehler beim API-Aufruf: {e}")
+        return None
+
+
+api_response = call_api()
 
 print(get_public_ip())
+print(api_response)
