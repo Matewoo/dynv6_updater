d51d8dd8490b3b5098c10f3cce17ef3f04a0b00b
Mateo Körber Belmonte <44673490+Matewoo@users.noreply.github.com>
2024-11-29 19:25:40 +0100

    add api_key.txt

--- ddns_updater.py
+++ ddns_updater.py
@@ -1,16 +1,25 @@
 import requests
 
-dynv6_username = "" # Example: "55cjZFwRJ7URYRchE8WY5UHdVRnB5l"
 dynv6_domain = "" # Example: "dynv6.net"
 
 def get_public_ip():
     response = requests.get("https://api.ipify.org?format=text")
     return response.text
 
+
+def get_api_key():
+    try:
+        with open("api_key.txt") as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        print("API-Schlüssel-Datei nicht gefunden.")
+        return None
+
+
 def call_api():
     
     try:
-        response = requests.get(f"https://dynv6.com/api/update?hostname={dynv6_domain}&token={dynv6_username}&ipv4=" + get_public_ip())
+        response = requests.get(f"https://dynv6.com/api/update?hostname={dynv6_domain}&token=" + get_api_key + "&ipv4=" + get_public_ip())
         response.raise_for_status()
         return response.text
     except requests.RequestException as e:
